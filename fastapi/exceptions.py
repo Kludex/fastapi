9a601a1b905e17566844fe0d22c30d3505b6c2fb
Marcelo Trylesinski <marcelotryle@gmail.com>
2023-06-10 12:36:40 +0200

    ♻️ Use HTTPException from Starlette

--- fastapi/exceptions.py
+++ fastapi/exceptions.py
@@ -1,20 +1,9 @@
-from typing import Any, Dict, Optional, Sequence, Type
+from typing import Any, Sequence, Type
 
 from pydantic import BaseModel, ValidationError, create_model
 from pydantic.error_wrappers import ErrorList
-from starlette.exceptions import HTTPException as StarletteHTTPException
+from starlette.exceptions import HTTPException as HTTPException  # noqa: F401
 from starlette.exceptions import WebSocketException as WebSocketException  # noqa: F401
-
-
-class HTTPException(StarletteHTTPException):
-    def __init__(
-        self,
-        status_code: int,
-        detail: Any = None,
-        headers: Optional[Dict[str, Any]] = None,
-    ) -> None:
-        super().__init__(status_code=status_code, detail=detail, headers=headers)
-
 
 RequestErrorModel: Type[BaseModel] = create_model("Request")
 WebSocketErrorModel: Type[BaseModel] = create_model("WebSocket")
